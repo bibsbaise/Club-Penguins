429d3b727e6600a41684098234c937a2465b51e7
GuiRodrr <10419633@mackenzista.com.br>
2025-09-28 13:36:32 -0300

    Create Perfil.java

---
+++ TG2/Perfil.java
@@ -0,0 +1,22 @@
+// Classe preliminar da classe Perfil, de acordo com o diagrama de classe de domínio
+public class Perfil {
+    
+    public String analisarVidas(int qtdVidas){ // Analisar quantidade de vidas do usuário
+        
+        String mOutAnalise;
+              
+        if (qtdVidas == 0){
+            mtOutClassificacao = "Ops, pinguim! Parece que suas vidas acabaram! Use suas moedas para comprar mais vidas!";
+        }
+        else if (qtdVidas == 1){
+            mtOutClassificacao = "Cuidado! Suas vidas estão acabando, pinguim! Tenha mais certeza nas próximas licoes, senão prepare o bolso!";
+        }
+        else{
+            mtOutClassificacao = "Pode ficar sereno e tranquilo, pinguim! Continue aprendendo.";
+        }
+
+        return mtOutClassificacao;
+        
+    }
+
+}
