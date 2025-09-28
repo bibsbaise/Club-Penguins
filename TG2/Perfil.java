2c723be564c70f264c8a5290a3253874eebbc351
GuiRodrr <10419633@mackenzista.com.br>
2025-09-28 15:23:24 -0300

    Update Perfil.java
    Teste 2 - Jenkins

--- TG2/Perfil.java
+++ TG2/Perfil.java
@@ -1,4 +1,4 @@
-// Adicionando comentario para testar Jenkins
+// Adicionando comentario para testar Jenkins Teste2
 // Classe preliminar da classe Perfil, de acordo com o diagrama de classe de domínio
 public class Perfil {
     
