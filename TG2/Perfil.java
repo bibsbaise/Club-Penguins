373c9e8e77fc9453e52f54466a61a72983a9bc3d
GuiRodrr <10419633@mackenzista.com.br>
2025-09-28 13:45:37 -0300

    Update Perfil.java

--- TG2/Perfil.java
+++ TG2/Perfil.java
@@ -1,22 +1,42 @@
 // Classe preliminar da classe Perfil, de acordo com o diagrama de classe de domínio
 public class Perfil {
     
-    public String analisarVidas(int qtdVidas){ // Analisar quantidade de vidas do usuário
+    public String analisarVidas(int qtdVidas){  // Analisar quantidade de vidas do usuário
         
         String mOutAnalise;
               
         if (qtdVidas == 0){
-            mtOutClassificacao = "Ops, pinguim! Parece que suas vidas acabaram! Use suas moedas para comprar mais vidas!";
+            mtOutAnalise = "Ops, pinguim! Parece que suas vidas acabaram! Use suas moedas para comprar mais vidas!";
         }
         else if (qtdVidas == 1){
-            mtOutClassificacao = "Cuidado! Suas vidas estão acabando, pinguim! Tenha mais certeza nas próximas licoes, senão prepare o bolso!";
+            mtOutAnalise = "Cuidado! Suas vidas estão acabando, pinguim! Tenha mais certeza nas próximas licoes, senão prepare o bolso!";
         }
         else{
-            mtOutClassificacao = "Pode ficar sereno e tranquilo, pinguim! Continue aprendendo e pinguinando.";
+            mtOutAnalise = "Pode ficar sereno e tranquilo, pinguim! Continue aprendendo e pinguinando.";
         }
 
-        return mtOutClassificacao;
+        return mtOutAnalise;
         
     }
 
+    public String analisarMoedas(int moedas){ // Analisar quantidade de moedas do usuário
+        
+        String mOutAnalise;
+              
+        if (moedas == 0){
+            mtOutAnalise = "SEM MOEDAS";
+        }
+        else if(moedas > 0){ // nao pode existir um caso onde a quantidade de moedas fique negativo.
+            mtOutAnalise = "COM MOEDAS";
+        }
+        else{
+            mtOutAnalise = "ERRO";
+        }
+
+
+        return mtOutAnalise;
+        
+    }
+
+    
 }
