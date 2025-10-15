8cb0919dc5f2a6d3c9c91e6d0b67ef658a37258e
Júlia Petersen <148015285+julia-petersen@users.noreply.github.com>
2025-10-15 19:16:42 +0000

    adicionado os componentes nas rotas, feita a estrutura inicial do componente

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+
 const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'sign-in', component: SignInComponent },
 ];
 
 @NgModule({
---
+++ frontend/src/app/sign-in/sign-in.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'sign-in-app',
+  templateUrl: './sign-in.component.html',
+  styleUrls: ['./sign-in.component.css']
+})
+
+export class SignInComponent{
+    
+}
