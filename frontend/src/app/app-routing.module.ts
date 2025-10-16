c822892c84b49fcdf56e5e6754b0397cd9950d12
Júlia Petersen <148015285+julia-petersen@users.noreply.github.com>
2025-10-16 22:06:24 +0000

    alterações na home e add profile

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { LevelsComponent } from './levels/levels.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' },
@@ -11,6 +12,7 @@ const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'sign-in', component: SignInComponent },
     { path: 'niveis/:modulo', component: LevelsComponent },
+    { path: 'profile', component: ProfileComponent }
 ];
 
 @NgModule({
